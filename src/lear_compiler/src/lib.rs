0420b4011ce3bd3ad0007a9ffd28ec9eb614cc80
DomiMartinGlogi <93612122+DomiMartinGlogi@users.noreply.github.com>
2023-12-02 11:34:01 +0100

    Intermittent Commit

--- src/lear_compiler/src/lib.rs
+++ src/lear_compiler/src/lib.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 use regex::Regex;
+use lear_instruction_set::init_instruction_set;
 
 
 /// Extracts the portion of the input string up to the first occurrence of a delimiter.
@@ -53,6 +54,48 @@ fn find_tokens(input: &str) -> Vec<String> {
     mat
 }
 
+fn compile_token(token: &str) -> Option<Vec<(String, Vec<&str>)>> {
+    // Get the instruction set
+    let ins_set = init_instruction_set();
+
+    // Split the token into separate lines
+    let mut lines: Vec<&str> = token.lines().collect();
+
+    // Ignore the first and last line
+    if lines.len() > 2 {
+        lines = lines[1..lines.len()-1].to_vec();
+    } else {
+        return None;
+    }
+
+    // Process each line separately
+    let mut instructions = Vec::new();
+    for line in lines {
+        // Split the line into instruction and arguments
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        let (inst_name, args) = parts.split_at(1);
+
+        // Check that the instruction name matches an instruction in the InstructionSet,
+        // and that the number of arguments matches the number expected for that instruction
+        match ins_set.get_instruction_by_name((*inst_name[0]).to_string()) {
+            Some(instruction) => {
+                if instruction.get_num_arg() == (args.len() as u8) {
+                    // Convert the instruction encoding to a hexadecimal string
+                    let encoding_str = format!("0x{:02X}", instruction.get_encoding());
+
+                    // If everything is fine, push the stringified instruction and arguments to the list
+                    instructions.push((encoding_str, args.to_vec()));
+                } else {
+                    println!("Incorrect number of arguments for instruction.");
+                    return None;
+                }
+            },
+            None => return None,
+        }
+    }
+    Some(instructions)
+}
+
 pub fn compile(path_string: String) {
     let source_path = Path::new(path_string.as_str());
     let dis = source_path.display();
@@ -68,4 +111,7 @@ pub fn compile(path_string: String) {
     };
 
     let tokens = find_tokens(&data);
+
+    let compiledTokens: Vec<&str>;
+
 }
